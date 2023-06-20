90a8e051d86e40b89d06ec61450f8496e30cd6ed
unknown <koustubh.dube@outlook.com>
2023-06-20 06:43:08 +0200

    changed technology paragraph

--- src/Technology.js
+++ src/Technology.js
@@ -1,7 +1,12 @@
 export default function Technology() {
   return (
     <div className="technology" id="technologyID">
-      <h2>We create chemical methods to capture carbon dioxide. </h2>
+      <h2>
+        We are a climate-tech start-up focused on carbon capture and removal.
+        Our mission is to help companies reduce their carbon footprint by
+        developing a novel technology that permanently captures carbon dioxide
+        with minimal energy requirements.{" "}
+      </h2>
     </div>
   );
 }
